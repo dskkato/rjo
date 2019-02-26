659e5f45c977bbf832f99b5a49a3b640bc6f3202
Daisuke Kato <kato.daisuke429@gmail.com>
2019-02-26 22:15:15 +0900

    add clap crate as dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+
+use std::env;
+
+fn run() -> Option<i64> {
+    let args: Vec<String> = env::args().collect();
+    Some(0)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = run();
+
+    match result {
+        Some(x) => println!("{:?}", x),
+        None => println!("None none"),
+    }
 }
