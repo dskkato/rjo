4735a19b9867106583a368d331359b4c68c3df33
Daisuke Kato <kato.daisuke429@gmail.com>
2019-02-27 09:07:02 +0900

    use clap as command parser

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 extern crate clap;
 use clap::{App, Arg, SubCommand};
 
-use std::env;
-
 fn run() -> Option<i64> {
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("rjo")
+        .version("0.1")
+        .author("Daisuke Kato <kato.daisuke429@gmail.com>")
+        .about("rjo is inspired by jo and gjo")
+        .arg(
+            Arg::with_name("config")
+                .short("c") 
+                .long("config")
+                .value_name("FILE")
+                .help("Sets a custom config file")
+                .takes_value(true),
+        )
+        .get_matches();
+    let config = matches.value_of("config").unwrap_or("default.conf");
+    println!("Value for config: {}", config);
     Some(0)
 }
 
