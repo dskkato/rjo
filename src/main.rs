04ee81557cb2d2790161bc5c8665f6f6c5829acd
Daisuke Kato <kato.daisuke429@gmail.com>
2019-03-03 00:49:13 +0900

    引数のオプション指定できるように修正

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,58 @@ fn run() -> Option<i64> {
         .version("0.1")
         .author("Daisuke Kato <kato.daisuke429@gmail.com>")
         .about("rjo is inspired by jo and gjo")
-        .arg(Arg::with_name("something").multiple(true))
+        .arg(
+            Arg::with_name("object")
+                .takes_value(true)
+                .multiple(true)
+                .required(true)
+                .conflicts_with("array"),
+        )
+        .arg(
+            Arg::with_name("array")
+                .short("a")
+                .takes_value(true)
+                .multiple(true)
+                .conflicts_with("object"),
+        )
+        .arg(Arg::with_name("pretty-print").short("p"))
         .get_matches();
-    let iterator = matches.values_of("something");
+
     let mut data = object! {};
-    for (i, el) in iterator.unwrap().enumerate() {
-        data[i.to_string()] = el.into();
+
+    if matches.is_present("object") {
+        let iterator = matches.values_of("object").unwrap();
+        for el in iterator {
+            let kv: Vec<&str> = el.splitn(2, '=').collect();
+            if kv.len() != 2 {
+                panic!("Argument {:?} is not k=v", el);
+            }
+
+            if kv[0].len() == 0 {
+                panic!("An empty key is not allowed {:?}", el);
+            }
+
+            let (key, value) = (kv[0], kv[1]);
+            data[key] = value.into();
+        }
+    } else if matches.is_present("array") {
+        let iterator = matches.values_of("array").unwrap();
+        for (i, el) in iterator.enumerate() {
+            data[i] = el.into();
+        }
     }
-    println!("{:#}", data);
-    Some(0)
+
+    if matches.is_present("pretty-print") {
+        println!("{:#}", data);
+    } else {
+        println!("{:#}", data.dump());
+    }
+    None
 }
 
 fn main() {
-    let result = run();
-
-    match result {
+    match run() {
         Some(x) => println!("{:?}", x),
-        None => println!("None none"),
+        None => print!(""),
     }
 }
