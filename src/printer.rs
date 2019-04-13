a9e993aa75b13da1b2799abbd7f549d6a9996a2b
Daisuke Kato <kato.daisuke429@gmail.com>
2019-04-13 22:45:24 +0900

    remove dupilicated new line word

--- src/printer.rs
+++ src/printer.rs
@@ -2,7 +2,7 @@ use syntect::dumps;
 use syntect::easy::HighlightLines;
 use syntect::highlighting::Style;
 use syntect::parsing::SyntaxSet;
-use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
+use syntect::util::as_24_bit_terminal_escaped;
 
 pub fn printer(s: &str) {
     let ps = SyntaxSet::load_defaults_newlines();
@@ -10,10 +10,8 @@ pub fn printer(s: &str) {
 
     let syntax = ps.find_syntax_by_extension("json").unwrap();
     let mut h = HighlightLines::new(syntax, &th);
-    for line in LinesWithEndings::from(s) {
-        // LinesWithEndings enables use of newlines mode
-        let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
-        let escaped = as_24_bit_terminal_escaped(&ranges[..], false);
-        println!("{}", escaped);
-    }
+
+    let ranges: Vec<(Style, &str)> = h.highlight(s, &ps);
+    let escaped = as_24_bit_terminal_escaped(&ranges, false);
+    println!("{}", escaped);
 }
